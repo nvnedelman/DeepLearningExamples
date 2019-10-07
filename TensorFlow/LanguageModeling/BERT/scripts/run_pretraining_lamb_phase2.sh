8c771ceed4db2c11dfba671075213b83c79bc829
Nicholas Edelman <nedelman@nvidia.com>
2019-10-07 15:37:14 -0500

    correcting data paths in pretraining scripts

--- TensorFlow/LanguageModeling/BERT/scripts/run_pretraining_lamb_phase2.sh
+++ TensorFlow/LanguageModeling/BERT/scripts/run_pretraining_lamb_phase2.sh
@@ -82,8 +82,8 @@ train_steps_phase2=$(expr $train_steps_phase2 \* $gbs_phase1 \/ $gbs_phase2) # A
 RESULTS_DIR_PHASE2=${RESULTS_DIR}/phase_2
 mkdir -m 777 -p $RESULTS_DIR_PHASE2
 
-INPUT_FILES="$DATA_DIR/tfrecord/lower_case_1_seq_len_${seq_len}_max_pred_${max_pred_per_seq}_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5_shard_1472_test_split_10/books_wiki_en_corpus/training"
-EVAL_FILES="$DATA_DIR/tfrecord/lower_case_1_seq_len_${seq_len}_max_pred_${max_pred_per_seq}_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5_shard_1472_test_split_10/books_wiki_en_corpus/test"
+INPUT_FILES="$DATA_DIR/tfrecord/lower_case_1_seq_len_${seq_len}_max_pred_${max_pred_per_seq}_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5_shard_256_test_split_20/books_wiki_en_corpus/training"
+EVAL_FILES="$DATA_DIR/tfrecord/lower_case_1_seq_len_${seq_len}_max_pred_${max_pred_per_seq}_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5_shard_256_test_split_20/books_wiki_en_corpus/test"
 
 #Check if all necessary files are available before training
 for DIR_or_file in $DATA_DIR $RESULTS_DIR $BERT_CONFIG ${PHASE1_CKPT}.meta; do
